2284bdb193c44c344935d00577a1ea3e4f5d362a
Marek Kurdej <marek@quasardb.net>
2017-01-12 14:23:30 +0100

    Add test for cluster.findNodeFor.

--- src/test/java/cluster/QdbClusterFindNodeForTest.java
+++ src/test/java/cluster/QdbClusterFindNodeForTest.java
@@ -2,6 +2,16 @@ import net.quasardb.qdb.*;
 import org.junit.*;
 
 public class QdbClusterFindNodeForTest {
+    @Test
+    public void returnsAddressAndPort() {
+        QdbCluster cluster = Helpers.createCluster();
+        String alias = Helpers.createUniqueAlias();
+
+        QdbNode node = cluster.findNodeFor(alias);
+        Assert.assertEquals("127.0.0.1", node.hostName());
+        Assert.assertEquals(DaemonRunner.port(), node.port());
+    }
+
     @Test(expected = QdbClusterClosedException.class)
     public void throwsClusterClosed_afterCallingClose() {
         QdbCluster cluster = Helpers.createCluster();
