7c9fe83846b8a3ce259851ee5a17447e30ae7879
Leon Mergen <leon@solatis.com>
2017-09-10 12:51:55 +0200

    Implements test that validates doubles

--- src/test/java/timeseries/QdbTimeSeriesDoubleTest.java
+++ src/test/java/timeseries/QdbTimeSeriesDoubleTest.java
@@ -17,4 +17,23 @@ public class QdbTimeSeriesDoubleTest {
         QdbDoubleColumnCollection data = Helpers.createDoubleColumnCollection(alias);
         series.insertDoubles(data);
     }
+
+    @Test
+    public void canGetResults_afterInsert() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeries series =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias)));
+
+        QdbDoubleColumnCollection data = Helpers.createDoubleColumnCollection(alias);
+        QdbTimeRange dataRange = data.range();
+        series.insertDoubles(data);
+
+        QdbTimeRangeCollection ranges = new QdbTimeRangeCollection();
+        ranges.add(new QdbTimeRange(dataRange.getBegin(),
+                                    new QdbTimespec(dataRange.getEnd().getValue().plusNanos(1))));
+
+        QdbDoubleColumnCollection results = series.getDoubles(alias, ranges);
+        assertThat(results, (is(data)));
+
+    }
 }
