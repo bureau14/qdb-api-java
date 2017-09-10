9a54f41ccc1c2e01b6e92ef4628aaffe79dbc417
Leon Mergen <leon@solatis.com>
2017-09-10 11:02:11 +0200

    Implements TimeRange and TimeRangeCollection

---
+++ src/main/java/net/quasardb/qdb/QdbTimeRange.java
@@ -0,0 +1,22 @@
+package net.quasardb.qdb;
+
+import net.quasardb.qdb.jni.*;
+
+public class QdbTimeRange {
+
+    protected QdbTimespec begin;
+    protected QdbTimespec end;
+
+    public QdbTimeRange (QdbTimespec begin, QdbTimespec end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public qdb_ts_range toNative() {
+        return new qdb_ts_range(begin.toNative(), end.toNative());
+    }
+
+    public String toString() {
+        return "QdbTimeRange (begin: " + this.begin.toString() + ", end: " + this.end.toString() + ")";
+    }
+}
