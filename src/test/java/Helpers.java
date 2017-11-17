9bdbbc721504d550af627b34fe7534ac72b22b51
Leon Mergen <leon@solatis.com>
2017-11-17 15:02:33 +0100

    Implements serialization tests

--- src/test/java/Helpers.java
+++ src/test/java/Helpers.java
@@ -8,6 +8,8 @@ public class Helpers {
     private static QdbCluster cluster = createCluster();
     private static long n = 1;
     public static final String RESERVED_ALIAS = "\u0000 is serialized as C0 80";
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
     public static final QdbCluster.SecurityOptions SECURITRY_OPTIONS =
         new QdbCluster.SecurityOptions("qdb-api-python",
                                        "SoHHpH26NtZvfq5pqm/8BXKbVIkf+yYiVZ5fQbq1nbcI=",
@@ -28,6 +30,16 @@ public class Helpers {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     public static void wait(double seconds) {
--- src/test/java/timeseries/QdbTimeSeriesValueTest.java
+++ src/test/java/timeseries/QdbTimeSeriesValueTest.java
@@ -14,23 +14,23 @@ public class QdbTimeSeriesValueTest {
     @Test
     public void canCreateDouble() throws Exception {
         double d = Helpers.randomDouble();
-        QdbTimeSeriesValue value = new QdbTimeSeriesValue.Double(d);
+        QdbTimeSeriesValue value = QdbTimeSeriesValue.createDouble(d);
         assertEquals(d, value.getDouble(), 0.01);
     }
 
     @Test
     public void canCreateBlob() throws Exception {
         ByteBuffer b = Helpers.createSampleData();
-        QdbTimeSeriesValue value = new QdbTimeSeriesValue.Blob(b);
+        QdbTimeSeriesValue value = QdbTimeSeriesValue.createBlob(b);
         assertEquals(b, value.getBlob());
     }
 
     @Test
     public void canCreateSafeBlob() throws Exception {
         QdbTimeSeriesValue value1 =
-            new QdbTimeSeriesValue.SafeBlob(Helpers.createSampleData());
+            QdbTimeSeriesValue.createSafeBlob(Helpers.createSampleData());
         QdbTimeSeriesValue value2 =
-            new QdbTimeSeriesValue.SafeBlob(value1.getBlob());
+            QdbTimeSeriesValue.createSafeBlob(value1.getBlob());
 
         assertEquals(value1, value2);
     }
@@ -38,22 +38,22 @@ public class QdbTimeSeriesValueTest {
     @Test(expected = QdbIncompatibleTypeException.class)
     public void throwsError_whenDoubleTypesDontMatch() throws Exception {
         ByteBuffer b = Helpers.createSampleData();
-        QdbTimeSeriesValue value = new QdbTimeSeriesValue.Blob(b);
+        QdbTimeSeriesValue value = QdbTimeSeriesValue.createBlob(b);
         value.getDouble(); // <-- throws
     }
 
     @Test(expected = QdbIncompatibleTypeException.class)
     public void throwsError_whenBlobTypesDontMatch() throws Exception {
         double d = Helpers.randomDouble();
-        QdbTimeSeriesValue value = new QdbTimeSeriesValue.Double(d);
+        QdbTimeSeriesValue value = QdbTimeSeriesValue.createDouble(d);
         value.getBlob(); // <-- throws
     }
 
     @Test
     public void canCompareDoubles() throws Exception {
-        QdbTimeSeriesValue value1 = new QdbTimeSeriesValue.Double(Helpers.randomDouble());
-        QdbTimeSeriesValue value2 = new QdbTimeSeriesValue.Double(Helpers.randomDouble());
-        QdbTimeSeriesValue value3 = new QdbTimeSeriesValue.Double(value1.getDouble());
+        QdbTimeSeriesValue value1 = QdbTimeSeriesValue.createDouble(Helpers.randomDouble());
+        QdbTimeSeriesValue value2 = QdbTimeSeriesValue.createDouble(Helpers.randomDouble());
+        QdbTimeSeriesValue value3 = QdbTimeSeriesValue.createDouble(value1.getDouble());
 
         assertFalse(value1.equals(value2));
         assertEquals(value1, value3);
@@ -64,9 +64,9 @@ public class QdbTimeSeriesValueTest {
     public void canCompareBlobs() throws Exception {
         ByteBuffer b1 = Helpers.createSampleData();
         ByteBuffer b2 = Helpers.createSampleData();
-        QdbTimeSeriesValue value1 = new QdbTimeSeriesValue.Blob(b1);
-        QdbTimeSeriesValue value2 = new QdbTimeSeriesValue.Blob(b2);
-        QdbTimeSeriesValue value3 = new QdbTimeSeriesValue.Blob(b1);
+        QdbTimeSeriesValue value1 = QdbTimeSeriesValue.createBlob(b1);
+        QdbTimeSeriesValue value2 = QdbTimeSeriesValue.createBlob(b2);
+        QdbTimeSeriesValue value3 = QdbTimeSeriesValue.createBlob(b1);
 
         assertFalse(value1.equals(value2));
         assertEquals(value1, value3);
@@ -77,9 +77,30 @@ public class QdbTimeSeriesValueTest {
     public void canCompareBlobAndDoubles() throws Exception {
         ByteBuffer b = Helpers.createSampleData();
         Double d = Helpers.randomDouble();
-        QdbTimeSeriesValue value1 = new QdbTimeSeriesValue.Blob(b);
-        QdbTimeSeriesValue value2 = new QdbTimeSeriesValue.Double(d);
+        QdbTimeSeriesValue value1 = QdbTimeSeriesValue.createBlob(b);
+        QdbTimeSeriesValue value2 = QdbTimeSeriesValue.createDouble(d);
 
         assertFalse(value1.equals(value2));
+    }
+
+    @Test
+    public void canSerialize_andDeserialize_Doubles() throws Exception {
+        double d = Helpers.randomDouble();
+        QdbTimeSeriesValue vBefore = QdbTimeSeriesValue.createDouble(d);
+
+        byte[] serialized = Helpers.serialize(vBefore);
+        QdbTimeSeriesValue vAfter = (QdbTimeSeriesValue)Helpers.deserialize(serialized, vBefore.getClass());
+
+        assertEquals(vBefore, vAfter);
+    }
+
+    @Test
+    public void canSerialize_andDeserialize_Blobs() throws Exception {
+        QdbTimeSeriesValue vBefore = QdbTimeSeriesValue.createBlob(Helpers.createSampleData());
+
+        byte[] serialized = Helpers.serialize(vBefore);
+        QdbTimeSeriesValue vAfter = (QdbTimeSeriesValue)Helpers.deserialize(serialized, vBefore.getClass());
+
+        assertEquals(vBefore, vAfter);
     }
 }
