140a619b9387d562709a86b6580ca89186129c1d
Leon Mergen <leon@solatis.com>
2017-11-18 13:29:53 +0100

    Tests adding 100k rows of doubles

--- src/test/java/timeseries/QdbTimeSeriesTableTest.java
+++ src/test/java/timeseries/QdbTimeSeriesTableTest.java
@@ -17,20 +17,36 @@ public class QdbTimeSeriesTableTest {
     }
 
     @Test
+    public void canFlushTable() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeriesTable table =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).table();
+
+        table.flush();
+    }
+
+    @Test
+    public void canCloseTable() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeriesTable table =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).table();
+
+        table.close();
+    }
+
+    @Test
     public void canInsertDoubleRow() throws Exception {
         String alias = Helpers.createUniqueAlias();
         QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias)));
         QdbTimeSeriesTable table = series.table();
 
-        QdbTimeSeriesValue[] values = new QdbTimeSeriesValue[1];
-        double value = Helpers.randomDouble();
-        values[0] = (QdbTimeSeriesValue.createDouble(value));
+        QdbTimeSeriesValue[] values = {
+            QdbTimeSeriesValue.createDouble(Helpers.randomDouble())
+        };
 
         QdbTimespec timestamp = new QdbTimespec(LocalDateTime.now());
-
         QdbTimeSeriesRow row = new QdbTimeSeriesRow(timestamp,
                                                     values);
-
         table.append(row);
         table.flush();
 
@@ -41,6 +57,123 @@ public class QdbTimeSeriesTableTest {
         QdbDoubleColumnCollection results = series.getDoubles(alias, ranges);
 
         assertThat(results.size(), (is(1)));
-        assertThat(results.get(0).getValue(), equalTo(value));
+        assertThat(results.get(0).getValue(), equalTo(values[0].getDouble()));
+    }
+
+    @Test
+    public void canInsertMultipleDoubleRows() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias)));
+        QdbTimeSeriesTable table = series.table();
+
+        int ROW_COUNT = 100000;
+        QdbTimeSeriesRow[] rows = new QdbTimeSeriesRow[ROW_COUNT];
+        for (int i = 0; i < rows.length; ++i) {
+            rows[i] =
+                new QdbTimeSeriesRow (new QdbTimespec(LocalDateTime.now()),
+                                      new QdbTimeSeriesValue[] {
+                                          QdbTimeSeriesValue.createDouble(Helpers.randomDouble())});
+            table.append(rows[i]);
+        }
+
+        table.flush();
+
+        QdbTimeRangeCollection ranges = new QdbTimeRangeCollection();
+        ranges.add(new QdbTimeRange(rows[0].getTimestamp(),
+                                    new QdbTimespec(rows[(rows.length - 1)].getTimestamp().asLocalDateTime().plusNanos(1))));
+
+        QdbDoubleColumnCollection results = series.getDoubles(alias, ranges);
+        assertThat(results.size(), (is(rows.length)));
+
+        for (int i = 0; i < rows.length; ++i) {
+            assertThat(results.get(i).getValue(), equalTo(rows[i].getValues()[0].getDouble()));
+        }
+
+    }
+
+    @Test
+    public void canInsertBlobRow() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Blob (alias)));
+        QdbTimeSeriesTable table = series.table();
+
+        QdbTimeSeriesValue[] values = {
+            QdbTimeSeriesValue.createSafeBlob(Helpers.createSampleData())
+        };
+
+        QdbTimespec timestamp = new QdbTimespec(LocalDateTime.now());
+        QdbTimeSeriesRow row = new QdbTimeSeriesRow(timestamp,
+                                                    values);
+        table.append(row);
+        table.flush();
+
+        QdbTimeRangeCollection ranges = new QdbTimeRangeCollection();
+        ranges.add(new QdbTimeRange(timestamp,
+                                    new QdbTimespec(timestamp.asLocalDateTime().plusNanos(1))));
+
+        QdbBlobColumnCollection results = series.getBlobs(alias, ranges);
+
+        assertThat(results.size(), (is(1)));
+        assertThat(results.get(0).getValue(), equalTo(values[0].getBlob()));
+    }
+
+    @Test
+    public void canInsertMultipleColumns() throws Exception {
+        String alias1 = Helpers.createUniqueAlias();
+        String alias2 = Helpers.createUniqueAlias();
+
+        QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias1),
+                                                                      new QdbColumnDefinition.Blob (alias2)));
+        QdbTimeSeriesTable table = series.table();
+
+        QdbTimeSeriesValue[] values = {
+            QdbTimeSeriesValue.createDouble(Helpers.randomDouble()),
+            QdbTimeSeriesValue.createBlob(Helpers.createSampleData())
+        };
+
+        QdbTimespec timestamp = new QdbTimespec(LocalDateTime.now());
+        QdbTimeSeriesRow row = new QdbTimeSeriesRow(timestamp,
+                                                    values);
+
+        table.append(row);
+        table.flush();
+
+        QdbTimeRangeCollection ranges = new QdbTimeRangeCollection();
+        ranges.add(new QdbTimeRange(timestamp,
+                                    new QdbTimespec(timestamp.asLocalDateTime().plusNanos(1))));
+
+        QdbDoubleColumnCollection results1 = series.getDoubles(alias1, ranges);
+        QdbBlobColumnCollection results2 = series.getBlobs(alias2, ranges);
+
+        assertThat(results1.size(), (is(1)));
+        assertThat(results2.size(), (is(1)));
+        assertThat(results1.get(0).getValue(), equalTo(values[0].getDouble()));
+        assertThat(results2.get(0).getValue(), equalTo(values[1].getBlob()));
+    }
+
+    @Test
+    public void tableIsFlushed_whenClosed() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+        QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias)));
+        QdbTimeSeriesTable table = series.table();
+
+        QdbTimeSeriesValue[] values = {
+            QdbTimeSeriesValue.createDouble(Helpers.randomDouble())
+        };
+
+        QdbTimespec timestamp = new QdbTimespec(LocalDateTime.now());
+        QdbTimeSeriesRow row = new QdbTimeSeriesRow(timestamp,
+                                                    values);
+        table.append(row);
+        table.close();
+
+        QdbTimeRangeCollection ranges = new QdbTimeRangeCollection();
+        ranges.add(new QdbTimeRange(timestamp,
+                                    new QdbTimespec(timestamp.asLocalDateTime().plusNanos(1))));
+
+        QdbDoubleColumnCollection results = series.getDoubles(alias, ranges);
+
+        assertThat(results.size(), (is(1)));
+        assertThat(results.get(0).getValue(), equalTo(values[0].getDouble()));
     }
 }
