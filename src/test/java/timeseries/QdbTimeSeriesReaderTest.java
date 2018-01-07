5eca58a0c683306f139542de1c1423c31d336c3f
Leon Mergen <leon@solatis.com>
2018-01-07 16:25:41 +0100

    Adds lots of reader tests

--- src/test/java/timeseries/QdbTimeSeriesReaderTest.java
+++ src/test/java/timeseries/QdbTimeSeriesReaderTest.java
@@ -13,8 +13,81 @@ public class QdbTimeSeriesReaderTest {
     public void canGetReader() throws Exception {
         String alias = Helpers.createUniqueAlias();
 
+        QdbTimeRange[] ranges = {
+            new QdbTimeRange(QdbTimespec.now(),
+                             QdbTimespec.now().plusNanos(1))
+        };
+        QdbTimeSeriesReader reader =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).tableReader(ranges);
+    }
+
+    @Test
+    public void canCloseReader() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+
+        QdbTimeRange[] ranges = {
+            new QdbTimeRange(QdbTimespec.now(),
+                             QdbTimespec.now().plusNanos(1))
+        };
+        QdbTimeSeriesReader reader =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).tableReader(ranges);
+        reader.close();
+    }
+
+    @Test(expected = QdbInvalidArgumentException.class)
+    public void readWithoutRanges_throwsException() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+
         QdbTimeRange[] ranges = {};
         QdbTimeSeriesReader reader =
             Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).tableReader(ranges);
+    }
+
+    @Test
+    public void canReadEmptyResult() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+
+        // These ranges should always be empty
+        QdbTimeRange[] ranges = {
+            new QdbTimeRange(QdbTimespec.now(),
+                             QdbTimespec.now().plusNanos(1))
+        };
+
+        QdbTimeSeriesReader reader =
+            Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias))).tableReader(ranges);
+
+        assertThat(reader.hasNext(), (is(false)));
+    }
+
+    @Test
+    public void canReadSingleValue_afterWriting() throws Exception {
+        String alias = Helpers.createUniqueAlias();
+
+        // Prepare single-column timeseries
+        QdbTimeSeries series = Helpers.createTimeSeries(Arrays.asList(new QdbColumnDefinition.Double (alias)));
+
+        // Prepare the single-column row
+        QdbTimeSeriesValue[] values = {
+            QdbTimeSeriesValue.createDouble(Helpers.randomDouble())
+        };
+
+        QdbTimespec timestamp = new QdbTimespec(LocalDateTime.now());
+        QdbTimeSeriesRow row = new QdbTimeSeriesRow(timestamp,
+                                                    values);
+
+        // These ranges should always be empty
+        QdbTimeRange[] ranges = {
+            new QdbTimeRange(timestamp,
+                             timestamp.plusNanos(1))
+        };
+
+        QdbTimeSeriesWriter writer = series.tableWriter();
+        writer.append(row);
+        writer.flush();
+
+        QdbTimeSeriesReader reader = series.tableReader(ranges);
+
+        assertThat(reader.hasNext(), (is(true)));
+
     }
 }
