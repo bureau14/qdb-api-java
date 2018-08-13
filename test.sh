1f59e67c751fc56b39bcddeda09f475c79e4852c
Marek Kurdej <marek@quasardb.net>
2018-08-13 11:01:26 +0200

    Bump version to 3.0.0.0-nightly.0.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="2.8.0-SNAPSHOT"
+QDB_API_VERSION="3.0.0-SNAPSHOT"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
