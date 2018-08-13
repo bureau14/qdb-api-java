dbca7951cd7d54e711fb29de84d7164dd592ad62
Marek Kurdej <marek@quasardb.net>
2018-08-13 10:48:51 +0200

    Case 2140 - [release] Fix set_version script.

--- scripts/release/set_version.sh
+++ scripts/release/set_version.sh
@@ -18,21 +18,21 @@ PATCH_VERSION=${WITHOUT_MINOR_VERSION%%.*}
 XYZ_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
 
 if [[ "${INPUT_VERSION}" == *-* ]] ; then
-    TAGS_VERSION=${INPUT_VERSION#*-}
-    TAGS_VERSION=${TAGS_VERSION/nightly/dev}
-    TAGS_VERSION=${TAGS_VERSION/./}
+    TAGS_VERSION="-SNAPSHOT"
 else
     TAGS_VERSION=
 fi
 
-if [[ -n "${TAGS_VERSION}" ]] ; then
-    FULL_XYZ_VERSION="${XYZ_VERSION}.${TAGS_VERSION}"
-else
-    FULL_XYZ_VERSION="${XYZ_VERSION}"
-fi
+FULL_XYZ_VERSION="${XYZ_VERSION}${TAGS_VERSION}"
 
 cd $(dirname -- $0)
 cd ${PWD}/../..
 
 sed -i -e 's/<version>*[^<]*<\/version>/<version>'"${FULL_XYZ_VERSION}"'<\/version>/' README.md
 sed -i -e 's/"net.quasardb.qdb:*[^"]*"/"net.quasardb.qdb:'"${FULL_XYZ_VERSION}"'"/' README.md
+
+# QDB_API_VERSION="2.8.0-SNAPSHOT"
+sed -i -e 's/\(QDB_API_VERSION="\)[^"]*\("\)/\1'"${FULL_XYZ_VERSION}"'\2/' test.sh
+
+sed -i -e '/<groupId>net.quasardb<\/groupId>/,/<\/\(properties\|dependency\)>/ s/<version>[0-9.]*[0-9]\(-SNAPSHOT\)\?<\/version>/<version>'"${FULL_XYZ_VERSION}"'<\/version>/' pom.xml pom-jni.xml pom-jni-arch.xml
+sed -i -e '/<groupId>net.quasardb<\/groupId>/,/<\/properties>/ s/<file>\([-a-zA-Z_/]*\)[0-9.]*[0-9]\(-SNAPSHOT\)\?\([-.${}a-zA-Z]*\)<\/file>/<file>\1'"${FULL_XYZ_VERSION}"'\3<\/file>/' pom.xml pom-jni.xml pom-jni-arch.xml
