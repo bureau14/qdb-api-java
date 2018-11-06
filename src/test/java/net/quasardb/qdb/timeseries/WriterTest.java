70807c6c514db441553c41b39e79cbf8298d4a7b
Leon Mergen <leon@solatis.com>
2018-11-06 10:12:25 +0100

    QDB-2170 Adds test case for new 'extraTables' functionality

--- src/test/java/net/quasardb/qdb/timeseries/WriterTest.java
+++ src/test/java/net/quasardb/qdb/timeseries/WriterTest.java
@@ -301,6 +301,78 @@ public class WriterTest {
     }
 
     @Test
+    public void canAddExtraColumnsAfterFlush() throws Exception {
+        Session session = Helpers.getSession();
+
+        String alias1 = Helpers.createUniqueAlias();
+        String alias2 = Helpers.createUniqueAlias();
+        Column[] definition = {
+            new Column.Double (alias1),
+            new Column.Blob (alias2)
+        };
+
+        Table table1 = Helpers.createTable(definition);
+        Writer writer = Table.writer(session, table1);
+
+        Timespec timestamp1 = new Timespec(LocalDateTime.now());
+        Timespec timestamp2 = timestamp1.plusNanos(1);
+
+        Value[] values1 = {
+            Value.createDouble(Helpers.randomDouble()),
+            Value.createBlob(Helpers.createSampleData())
+        };
+
+        Value[] values2table1 = {
+            Value.createDouble(Helpers.randomDouble()),
+            Value.createBlob(Helpers.createSampleData())
+        };
+        Value[] values2table2 = {
+            Value.createDouble(Helpers.randomDouble()),
+            Value.createBlob(Helpers.createSampleData())
+        };
+        Value[] values2 = {
+            values2table1[0],
+            values2table1[1],
+            values2table2[0],
+            values2table2[1]
+        };
+
+        Row row1 = new Row(timestamp1, values1);
+        Row row2 = new Row(timestamp2, values2);
+
+        writer.append(row1);
+        writer.flush();
+
+        Table table2 = Helpers.createTable(definition);
+        writer.extraTables(table2);
+        writer.append(row2);
+        writer.flush();
+
+        TimeRange[] ranges = {
+            new TimeRange(timestamp1, timestamp1.plusNanos(2))
+        };
+
+        Reader reader1 = Table.reader(session, table1.getName(), ranges);
+        assertThat(reader1.hasNext(), (is(true)));
+
+        Row table1row1 = reader1.next();
+        assertThat(table1row1, (is(row1)));
+
+        assertThat(reader1.hasNext(), (is(true)));
+        Row table1row2 = reader1.next();
+        assertThat(Arrays.equals(table1row2.getValues(), values2table1), (is(true)));
+        assertThat(table1row2.getTimestamp(), (is(timestamp2)));
+        assertThat(reader1.hasNext(), (is(false)));
+
+        Reader reader2 = Table.reader(session, table2.getName(), ranges);
+        assertThat(reader2.hasNext(), (is(true)));
+        Row table2row2 = reader2.next();
+        assertThat(Arrays.equals(table2row2.getValues(), values2table2), (is(true)));
+        assertThat(table2row2.getTimestamp(), (is(timestamp2)));
+        assertThat(reader2.hasNext(), (is(false)));
+    }
+
+    @Test
     public void writerIsFlushed_whenClosed() throws Exception {
         String alias = Helpers.createUniqueAlias();
         Column[] definition = {
