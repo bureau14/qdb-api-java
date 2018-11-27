ccc49e0c7af3d8fad7942bc6ed861b4b049c68cf
Leon Mergen <leon@solatis.com>
2018-11-27 10:11:04 +0100

    3.0.0->3.1.0

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.0.0-SNAPSHOT"
+QDB_API_VERSION="3.1.0-SNAPSHOT"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
@@ -23,4 +23,5 @@ mvn \
     -Dqdbd.port=28360 \
     -Dqdbd.secure.port=28361 \
     -Dqdb.api.version="${QDB_API_VERSION}" \
+    '-Dtest=WriterTest#canAsyncInsertDoubleRow*' \
     test
