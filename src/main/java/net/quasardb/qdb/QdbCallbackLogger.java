5f12b8bc1fecc2a1731f7b803d67c8736f11df52
vianney <vianney.plotton@gmail.com>
2019-06-14 07:15:30 +0200

    Add callback logger.

---
+++ src/main/java/net/quasardb/qdb/QdbCallbackLogger.java
@@ -0,0 +1,8 @@
+package net.quasardb.qdb;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class QdbCallbackLogger {
+    public static final Logger Log = LogManager.getLogger("qdb.logger");
+}
