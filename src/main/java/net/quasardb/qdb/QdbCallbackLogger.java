9088dfd354fc9109a272540ec0d4b658a2c19cdb
vianney <vianney.plotton@gmail.com>
2019-06-14 22:17:33 +0200

    Remove unnecessary file.

--- src/main/java/net/quasardb/qdb/QdbCallbackLogger.java
+++
@@ -1,8 +0,0 @@
-package net.quasardb.qdb;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class QdbCallbackLogger {
-    public static final Logger Log = LogManager.getLogger("qdb.logger");
-}
