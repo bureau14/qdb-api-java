0bbe5c6ca87c8bfe960d1c78c0d11b11fa72781e
terngkub <terngnattapol@gmail.com>
2020-08-03 18:43:12 +0200

    Bump version to 3.11.0.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -x
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.9.0-SNAPSHOT"
+QDB_API_VERSION="3.11.0"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
