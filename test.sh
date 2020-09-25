8ac65245b3ae4662192328c3d48f930a25b44eb4
vianney <vianney.p@outlook.com>
2020-09-25 09:21:25 +0200

    Bump version to 3.9.4.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -x
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.9.4-SNAPSHOT"
+QDB_API_VERSION="3.9.4"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
