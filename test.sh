b2cc7284b7c7b937972df23e2eddb9bc950cb2ba
Leon Mergen <leon@solatis.com>
2021-09-28 11:07:00 +0200

    Bump version to 3.10.0-nightly.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -x
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.9.10-SNAPSHOT"
+QDB_API_VERSION="3.10.0-SNAPSHOT"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
