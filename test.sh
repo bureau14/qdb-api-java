02ab7d5d2ef751de998b8fa7c4d7586cc2c6b903
vianney <vianney.p@outlook.com>
2022-03-31 15:35:10 +0200

    Bump version to 3.13.3.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -x
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.13.3-SNAPSHOT"
+QDB_API_VERSION="3.13.3"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
