5bcfe33c263575c67c1f8ff6156ffb358c8dba14
Leon Mergen <leon@solatis.com>
2022-11-24 14:56:54 +0700

    Bump version to 3.13.6.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -x
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.13.6-SNAPSHOT"
+QDB_API_VERSION="3.13.6"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
